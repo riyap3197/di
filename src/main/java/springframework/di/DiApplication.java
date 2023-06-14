72bf686c1ceb6cd79b3bcde53337c5034843dcac
riyap3197 <riyap3197@gmail.com>
2023-06-13 22:46:46 -0400

    Bean Scopes

--- src/main/java/springframework/di/DiApplication.java
+++ src/main/java/springframework/di/DiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import springframework.di.controllers.*;
+import springframework.di.services.PrototypeBean;
+import springframework.di.services.SingletonBean;
 
 @SpringBootApplication
 public class DiApplication {
@@ -35,6 +37,17 @@ public class DiApplication {
 		System.out.println("------- Constructor");
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
+
+		System.out.println("------- Bean Scopes ----------");
+		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
+
+		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
 	}
 
 }
